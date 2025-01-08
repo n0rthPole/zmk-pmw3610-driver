18918c0dffbe561aff5c20fe66bfb0647dc0eb1b
n0rthPole <icejuice2202@gmail.com>
2025-01-08 17:10:23 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -674,7 +674,7 @@ static int pmw3610_report_data(const struct device *dev) {
         y = -y;
     }
 
-#if AUTOMOUSE_LAYER > 0
+//#if AUTOMOUSE_LAYER > 0
     if (input_mode == MOVE && (abs(x)+abs(y) > 0.6) &&
             (automouse_triggered || zmk_keymap_highest_layer_active() != AUTOMOUSE_LAYER)
     ) {
