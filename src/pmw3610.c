d27d7d15bad8a1ac9c0bbbc119387ff9bc440853
n0rthPole <icejuice2202@gmail.com>
2025-01-08 17:14:49 +0900

    Update pmw3610.c
    automouse_invalid_test

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -674,8 +674,8 @@ static int pmw3610_report_data(const struct device *dev) {
         y = -y;
     }
 
-//#if AUTOMOUSE_LAYER > 0
-    if (input_mode == MOVE && (abs(x)+abs(y) > 0.6) &&
+#if AUTOMOUSE_LAYER > 0
+    //if (input_mode == MOVE && (abs(x)+abs(y) > 0.6) &&
             (automouse_triggered || zmk_keymap_highest_layer_active() != AUTOMOUSE_LAYER)
     ) {
         activate_automouse_layer();
